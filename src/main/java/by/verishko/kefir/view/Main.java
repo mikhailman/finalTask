832b20fcce7c400485609d30cdebb70748f5a33b
Michael <mishanya.v91@mail.ru>
2020-05-14 19:07:02 +0300

    .

--- src/main/java/by/verishko/kefir/view/Main.java
+++ src/main/java/by/verishko/kefir/view/Main.java
@@ -5,6 +5,7 @@ import by.verishko.kefir.dao.connect.ConnectionPool;
 public class Main {
     public static void main(String[] args) {
 //        ConnectionPool connectionPool = ConnectionPool.getInstance();
+
         System.out.println(ConnectionPool.getInstance().toString());
     }
 }
