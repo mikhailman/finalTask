ca324b6e90006190c0f62dda45e6e4a285176f0c
Michael <mishanya.v91@mail.ru>
2020-05-24 21:33:31 +0300

    add test_db

---
+++ src/test/resources/sql/create_db.sql
@@ -0,0 +1,121 @@
+CREATE SCHEMA IF NOT EXISTS kefir_test DEFAULT CHARACTER SET utf8;
+USE kefir_test;
+
+
+CREATE TABLE IF NOT EXISTS kefir_test.users
+(
+    `id`                INT(11)      NOT NULL AUTO_INCREMENT,
+    `role`              TINYINT(4)   NOT NULL DEFAULT 2,
+    /*
+     * 1 - администратор (Role.ADMINISTRATOR)
+     * 2 - пользователь (Role.USER)
+     */
+    `login`             VARCHAR(45)  NOT NULL UNIQUE,
+    `password`          VARCHAR(256) NOT NULL,
+    `email`             VARCHAR(256) NOT NULL UNIQUE,
+    `phone`             BIGINT       NULL,
+    `name`              VARCHAR(45)  NULL,
+    `surname`           VARCHAR(45)  NULL,
+    `status`            TINYINT(1)   NOT NULL DEFAULT 1,
+    `date_registration` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 14
+    DEFAULT CHARACTER SET = utf8;
+
+
+
+CREATE TABLE IF NOT EXISTS kefir_test.category
+(
+    `id`   INT(11)     NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(45) NULL,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS kefir_test.products
+(
+    `id`            INT(11)        NOT NULL AUTO_INCREMENT,
+    `name`          VARCHAR(100)   NULL,
+    `description`   VARCHAR(2000)  NULL,
+    `price`         DECIMAL(10, 0) NULL,
+    `date_creation` DATE           NULL,
+    `users_id`      INT(11)        NOT NULL,
+    `category_id`   INT(11)        NOT NULL,
+    `location_id`   INT(11)        NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_products_users1_idx` (`users_id` ASC),
+    INDEX `fk_products_category1_idx` (`category_id` ASC),
+    INDEX `fk_products_location1_idx` (`location_id` ASC),
+    CONSTRAINT `fk_products_users1`
+        FOREIGN KEY (`users_id`)
+            REFERENCES kefir_test.users (`id`)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+    CONSTRAINT `fk_products_category1`
+        FOREIGN KEY (`category_id`)
+            REFERENCES kefir_test.category (`id`)
+            ON DELETE RESTRICT
+            ON UPDATE RESTRICT,
+    CONSTRAINT `fk_products_location1`
+        FOREIGN KEY (`location_id`)
+            REFERENCES kefir_test.location (`id`)
+            ON DELETE RESTRICT
+            ON UPDATE RESTRICT
+)
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 12
+    DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS kefir_test.images
+(
+    `id`          INT(11)       NOT NULL AUTO_INCREMENT,
+    `name`        VARCHAR(45)   NULL,
+    `image_path`  VARCHAR(4096) NULL,
+    `products_id` INT(11)       NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `products_id_idx` (`products_id` ASC),
+    CONSTRAINT `products_id`
+        FOREIGN KEY (`products_id`)
+            REFERENCES kefir_test.products (`id`)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+)
+    ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS kefir_test.comments
+(
+    `id`          INT(11)       NOT NULL AUTO_INCREMENT,
+    `name`        VARCHAR(45)   NULL,
+    `body`        VARCHAR(4096) NULL,
+    `users_id`    INT(11)       NOT NULL,
+    `products_id` INT(11)       NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `user_id_idx` (`users_id` ASC),
+    INDEX `fk_product_id_idx` (`products_id` ASC),
+    CONSTRAINT `fk_users_id`
+        FOREIGN KEY (`users_id`)
+            REFERENCES kefir_test.users (`id`)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+    CONSTRAINT `fk_product_id`
+        FOREIGN KEY (`products_id`)
+            REFERENCES kefir_test.products (`id`)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+)
+    ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS kefir_test.location
+(
+    `id`   INT(11)      NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NULL,
+    `city` VARCHAR(45)  NULL,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB;
---
+++ src/test/resources/sql/fill_db.sql
@@ -0,0 +1,42 @@
+USE kefir_test;
+
+INSERT INTO `kefir_test`.`users`(`id`, `role`, `login`, `password`, `email`, `phone`, `name`, `surname`, `status`,
+                                 `date_registration`)
+VALUES (DEFAULT, 1, 'admin', 'password', 'admin@mail.ru', '375292020327', 'valera', 'valerin', DEFAULT, DEFAULT);
+INSERT INTO `kefir_test`.`users`(`id`, `role`, `login`, `password`, `email`, `phone`, `name`, `surname`, `status`,
+                                 `date_registration`)
+VALUES (DEFAULT, 2, 'user1', 'password', 'user1@mail.ru', '375292020328', 'katya', 'katina', DEFAULT, DEFAULT);
+INSERT INTO `kefir_test`.`users`(`id`, `role`, `login`, `password`, `email`, `phone`, `name`, `surname`, `status`,
+                                 `date_registration`)
+VALUES (DEFAULT, 2, 'user2', 'password', 'user2@mail.ru', '375292020329', 'lera', 'lerina', DEFAULT, DEFAULT);
+INSERT INTO `kefir_test`.`users`(`id`, `role`, `login`, `password`, `email`, `phone`, `name`, `surname`, `status`,
+                                 `date_registration`)
+VALUES (DEFAULT, 2, 'user3', 'password', 'user3@mail.ru', '375292020320', 'nina', 'ninina', DEFAULT, DEFAULT);
+INSERT INTO `kefir_test`.`category` (`id`, `name`)
+VALUES (DEFAULT, 'Бытовая техника');
+INSERT INTO `kefir_test`.`category` (`id`, `name`)
+VALUES (DEFAULT, 'Крупная бытовая техника');
+INSERT INTO `kefir_test`.`category` (`id`, `name`)
+VALUES (DEFAULT, 'Мелкая бытовая техника');
+INSERT INTO `kefir_test`.`location` (`id`, `name`, `city`)
+VALUES (DEFAULT, 'Nezavisimosti,4', 'Minsk');
+INSERT INTO `kefir_test`.`location` (`id`, `name`, `city`)
+VALUES (DEFAULT, 'Orlovskaya,19', 'Pinsk');
+INSERT INTO `kefir_test`.`products` (`id`, `name`, `description`, `price`, `date_creation`, `users_id`, `category_id`,
+                                     `location_id`)
+VALUES (DEFAULT, 'table', 'my table description', 11.1, '2019-11-14', 15, 2, 1);
+INSERT INTO `kefir_test`.`products` (`id`, `name`, `description`, `price`, `date_creation`, `users_id`, `category_id`,
+                                     `location_id`)
+VALUES (DEFAULT, 'phone', 'my phone description', 33.3, '2019-11-14', 16, 3, 2);
+INSERT INTO `kefir_test`.`images` (`id`, `name`, `image_path`, `products_id`)
+VALUES (DEFAULT, 'table_image',
+        'https://www.google.by/search?q=стол+фото&client=opera&hs=b3b&sxsrf=ACYBGNT-QCVEDmNz1mGANfFTZkziN3WBCg:1573734183969&tbm=isch&source=iu&ictx=1&fir=K-CJyuN6XssemM%253A%252C9TW2I9_CBvKbJM%252C_&vet=1&usg=AI4_-kSGySRNI_9oigFov9rEKHB0s_CMWA&sa=X&ved=2ahUKEwjYnYbY2OnlAhVhlosKHbcyCwMQ9QEwAHoECAcQBA#imgrc=K-CJyuN6XssemM:',
+        13);
+INSERT INTO `kefir_test`.`images` (`id`, `name`, `image_path`, `products_id`)
+VALUES (DEFAULT, 'phone_image',
+        'https://www.google.by/search?q=телефон+фото&client=opera&sxsrf=ACYBGNTUofwGX72SHK-zoHCFeIIbTkpLAg:1573734718226&tbm=isch&source=iu&ictx=1&fir=c5Gbijx0SiGfTM%253A%252CaNWqGzRLrYhaDM%252C_&vet=1&usg=AI4_-kTJ4PmXpEy30O1clKW7Z6xFZTQ_bA&sa=X&ved=2ahUKEwjv2ObW2unlAhVGAxAIHaAdBI4Q9QEwCnoECAYQGA#imgrc=c5Gbijx0SiGfTM:',
+        13);
+INSERT INTO `kefir_test`.`comments` (`id`, `name`, `body`, `users_id`, `products_id`)
+VALUES (DEFAULT, 'table_comment', 'this table bla-bla', 16, 12);
+INSERT INTO `kefir_test`.`comments` (`id`, `name`, `body`, `users_id`, `products_id`)
+VALUES (DEFAULT, 'phone_comment', 'this phone bla-bla', 15, 13);
